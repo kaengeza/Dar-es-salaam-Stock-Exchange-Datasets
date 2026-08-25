02a21a1fe869d39a63f09c829920daa6d41f930a
Kaengeza <chausikukassimu1@gmail.com>
2026-08-26 01:15:55 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,6 +7,44 @@ import requests
 URL = "https://dse.co.tz/"
 FILE = "ohlcv.csv"
 
+import pandas as pd
+
+class cleaning:
+    def __init__(self, df: pd.DataFrame):
+        ...
+    def cleaner(df: pd.DataFrame)-> pd.DataFrame:
+        df["Symbol"] = (df["Symbol"].astype(str).str.strip().str.upper())
+        df["Date"]   = pd.to_datetime(df["Date"], errors="coerce")
+        df["Change"] = (df["Change"]
+                        .astype("string")
+                        .str.extract(r"([-+]?\d+(?:\.\d+)?)", expand=False)
+                        .astype(float)
+                       )
+        df["% Change"] = ((df["Change"]/100) * df['Close'])
+        
+        df           = df.rename(columns={"Symbol": "Ticker", "Turn over": "Turnover", "MCAP (TZS 'B)": "mktcap"})
+        df           = df.rename(columns={"Out Standing Bid": "Bids", "Out Standing Offer": "Offers"})
+        df           = df.rename(columns={col: col.lower() for col in df.columns})
+        df           = df.drop_duplicates(inplace=False)
+        
+        for col in df.columns.difference(["date", "ticker", "change"]):
+            if col in df.columns:
+                df[col] = (
+                    df[col]
+                    .astype(str)
+                    .str.replace(",", "", regex=False)
+                    .str.replace("TZS", "", regex=False)
+                    .str.strip()
+                    .pipe(pd.to_numeric, errors="coerce")
+                 )
+
+        columns = ['date', 'ticker','high', 'low', 'open', 
+                   'prev close', 'close', 'change', '% change',
+                   'volume', 'turnover', 'deals', 'bids', 
+                   'offers', 'mktcap'
+                    ]
+        
+        return df[columns].copy()
 
 def load_existing_data():
     if Path(FILE).exists():
@@ -66,6 +104,8 @@ def main():
         .sort_values(["Date", "Symbol"])
         .reset_index(drop=True)
     )
+
+    combined= cleaning.cleaner(combined)
 
     # Save
     combined.to_csv(FILE, index=False)
