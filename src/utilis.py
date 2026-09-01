5cc2c37ad329a515b7a54c29e410d9a6a31f6206
Kaengeza <chausikukassimu1@gmail.com>
2026-09-01 15:31:53 +0300

    Delete src/utilis.py

--- src/utilis.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from typing import Union
-import pandas as pd
-
-def existing_data(file: Union[Path] = "dataset/ohlcv.csv")-> pd.DataFrame():
-    if Path(file).exists():
-        return pd.read_csv(file)
-    return pd.DataFrame()
